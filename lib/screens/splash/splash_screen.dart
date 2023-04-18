96a06a97494b5c6d4c1ccaa08eab38e0e939ab8f
muhammad <786hamza.hamza@gmail.com>
2023-04-18 10:49:00 +0200

    Firebase done

---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          child: Image.asset('assets/images/app_splash_logo.png'),
+        ),
+      ),
+    );
+  }
+}
